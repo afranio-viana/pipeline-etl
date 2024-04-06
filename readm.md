8d7f90a48f4439002a5eb7a77a59c4b44a8a003c
afranio-viana <afranio.esquerdo.viana@gmail.com>
2024-04-05 20:36:29 -0400

    Adicionando ETL

---
+++ readm.md
@@ -0,0 +1,3 @@
+## Descrição
+
+O objetivo principal deste projeto é aplicar os conhecimentos adquiridos no curso [Pipeline de dados: combinando Python e orientação a objeto](https://cursos.alura.com.br/course/pipeline-dados-combinando-python-orientacao-objeto)  para construir um pipeline de dados eficiente e reutilizável, utilizando as melhores práticas de programação em Python.
---
+++ scripts/fusao_mercado_mar.py
@@ -0,0 +1,35 @@
+from processamento_dados import Dados
+
+
+#Extract
+path_json = 'data_raw/dados_empresaA.json'
+path_csv = 'data_raw/dados_empresaB.csv'
+
+dados_empresaA = Dados(path_json,'json')
+dados_empresaB = Dados(path_csv,'csv')
+
+
+
+#Transform
+
+key_mapping = {
+    'Nome do Item': 'Nome do Produto',
+    'Classificação do Produto': 'Categoria do Produto',
+    'Valor em Reais (R$)': 'Preço do Produto (R$)',
+    'Quantidade em Estoque': 'Quantidade em Estoque',
+    'Nome da Loja': 'Filial',
+    'Data da Venda': 'Data da Venda'
+}
+
+dados_empresaB.rename_columns(key_mapping)
+dados_fusao = Dados.join_data(dados_empresaA,dados_empresaB)
+
+#print(dados_fusao.dados[4])
+
+
+#Load
+
+path_dados_combinados = 'data_processed/dados_combinados.csv'
+dados_fusao.salvando_dados(path_dados_combinados)
+
+
